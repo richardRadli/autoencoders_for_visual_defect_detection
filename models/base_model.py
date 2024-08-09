9f209859b9d0ad7db3c8cbd51e1a6f4202a6a646
Richárd Rádli <77007078+richardRadli@users.noreply.github.com>
2024-08-09 20:42:00 +0200

    Updated models.

--- models/base_model.py
+++ models/base_model.py
@@ -6,141 +6,105 @@ class AutoEncoder(nn.Module):
     def __init__(self, cfg):
         super(AutoEncoder, self).__init__()
 
+        # Extract configuration values
+        input_channel = cfg.get("input_channel")
+        flc = cfg.get("flc")
+        kernel_size = cfg.get("kernel_size")
+        stride = cfg.get("stride")
+        padding = cfg.get("padding")
+        alpha_slope = cfg.get("alpha_slope")
+        latent_space_dimension = cfg.get("latent_space_dimension")
+
+        # Encoder layers
         self.encoder = nn.Sequential(
-            nn.Conv2d(in_channels=cfg.get("input_channel"),
-                      out_channels=cfg.get("flc")[0],
-                      kernel_size=cfg.get("kernel_size")[1],
-                      stride=cfg.get("stride")[1],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope"),
-                         inplace=True),
-            nn.Conv2d(in_channels=cfg.get("flc")[0],
-                      out_channels=cfg.get("flc")[0],
-                      kernel_size=cfg.get("kernel_size")[1],
-                      stride=cfg.get("stride")[1],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope"),
-                         inplace=True),
-            nn.Conv2d(in_channels=cfg.get("flc")[0],
-                      out_channels=cfg.get("flc")[0],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[0],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope"),
-                         inplace=True),
-            nn.Conv2d(in_channels=cfg.get("flc")[0],
-                      out_channels=cfg.get("flc")[1],
-                      kernel_size=cfg.get("kernel_size")[1],
-                      stride=cfg.get("stride")[1],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope"),
-                         inplace=True),
-            nn.Conv2d(in_channels=cfg.get("flc")[1],
-                      out_channels=cfg.get("flc")[1],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[0],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope"),
-                         inplace=True),
-            # Conv-6
-            nn.Conv2d(in_channels=cfg.get("flc")[1],
-                      out_channels=cfg.get("flc")[2],
-                      kernel_size=cfg.get("kernel_size")[1],
-                      stride=cfg.get("stride")[1],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope"),
-                         inplace=True),
-            # Conv-7
-            nn.Conv2d(in_channels=cfg.get("flc")[2],
-                      out_channels=cfg.get("flc")[1],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[0],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope"),
-                         inplace=True),
-            # Conv-8
-            nn.Conv2d(in_channels=cfg.get("flc")[1],
-                      out_channels=cfg.get("flc")[0],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[0],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope"),
-                         inplace=True),
-            # Conv-9
-            nn.Conv2d(in_channels=cfg.get("flc")[0],
-                      out_channels=cfg.get("latent_space_dimension"),
-                      kernel_size=cfg.get("kernel_size")[2],
-                      stride=cfg.get("stride")[0],
-                      padding=cfg.get("padding")[0])
+            self._conv_block(input_channel, flc[0], kernel_size[1], stride[1], padding[1], alpha_slope),
+            self._conv_block(flc[0], flc[0], kernel_size[1], stride[1], padding[1], alpha_slope),
+            self._conv_block(flc[0], flc[0], kernel_size[0], stride[0], padding[1], alpha_slope),
+            self._conv_block(flc[0], flc[1], kernel_size[1], stride[1], padding[1], alpha_slope),
+            self._conv_block(flc[1], flc[1], kernel_size[0], stride[0], padding[1], alpha_slope),
+            self._conv_block(flc[1], flc[2], kernel_size[1], stride[1], padding[1], alpha_slope),
+            self._conv_block(flc[2], flc[1], kernel_size[0], stride[0], padding[1], alpha_slope),
+            self._conv_block(flc[1], flc[0], kernel_size[0], stride[0], padding[1], alpha_slope),
+            nn.Conv2d(flc[0], latent_space_dimension, kernel_size[2], stride[0], padding[0])
         )
 
+        # Decoder layers
         self.decoder = nn.Sequential(
-            # DeConv-9
-            nn.ConvTranspose2d(in_channels=cfg.get("latent_space_dimension"),
-                               out_channels=cfg.get("flc")[0],
-                               kernel_size=cfg.get("kernel_size")[2],
-                               stride=cfg.get("stride")[0],
-                               padding=cfg.get("padding")[0]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope"),
-                         inplace=True),
-            # DeConv-8
-            nn.Conv2d(in_channels=cfg.get("flc")[0],
-                      out_channels=cfg.get("flc")[1],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[0],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope"), inplace=True),
-            # DeConv-7
-            nn.Conv2d(in_channels=cfg.get("flc")[1],
-                      out_channels=cfg.get("flc")[2],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[0], padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope"), inplace=True),
-            # DeConv-6
-            nn.ConvTranspose2d(in_channels=cfg.get("flc")[2],
-                               out_channels=cfg.get("flc")[1],
-                               kernel_size=cfg.get("kernel_size")[1],
-                               stride=cfg.get("stride")[1],
-                               padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope"), inplace=True),
-            # DeConv-5
-            nn.Conv2d(in_channels=cfg.get("flc")[1],
-                      out_channels=cfg.get("flc")[1],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[0],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope"), inplace=True),
-            # DeConv-4
-            nn.ConvTranspose2d(in_channels=cfg.get("flc")[1],
-                               out_channels=cfg.get("flc")[0],
-                               kernel_size=cfg.get("kernel_size")[1],
-                               stride=cfg.get("stride")[1],
-                               padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope"), inplace=True),
-            # DeConv-3
-            nn.Conv2d(in_channels=cfg.get("flc")[0],
-                      out_channels=cfg.get("flc")[0],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[0],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope"), inplace=True),
-            # DeConv-2
-            nn.ConvTranspose2d(in_channels=cfg.get("flc")[0],
-                               out_channels=cfg.get("flc")[0],
-                               kernel_size=cfg.get("kernel_size")[1],
-                               stride=cfg.get("stride")[1],
-                               padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope"), inplace=True),
-            # DeConv-1
-            nn.ConvTranspose2d(in_channels=cfg.get("flc")[0],
-                               out_channels=cfg.get("input_channel"),
-                               kernel_size=cfg.get("kernel_size")[1],
-                               stride=cfg.get("stride")[1],
-                               padding=cfg.get("padding")[1]),
+            self._deconv_block(latent_space_dimension, flc[0], kernel_size[2], stride[0], padding[0], alpha_slope),
+            self._conv_block(flc[0], flc[1], kernel_size[0], stride[0], padding[1], alpha_slope),
+            self._conv_block(flc[1], flc[2], kernel_size[0], stride[0], padding[1], alpha_slope),
+            self._deconv_block(flc[2], flc[1], kernel_size[1], stride[1], padding[1], alpha_slope),
+            self._conv_block(flc[1], flc[1], kernel_size[0], stride[0], padding[1], alpha_slope),
+            self._deconv_block(flc[1], flc[0], kernel_size[1], stride[1], padding[1], alpha_slope),
+            self._conv_block(flc[0], flc[0], kernel_size[0], stride[0], padding[1], alpha_slope),
+            self._deconv_block(flc[0], flc[0], kernel_size[1], stride[1], padding[1], alpha_slope),
+            nn.ConvTranspose2d(flc[0], input_channel, kernel_size[1], stride[1], padding[1]),
             nn.Sigmoid()
         )
 
-    def forward(self, x: torch.Tensor):
+    @staticmethod
+    def _conv_block(
+            in_channels: int, out_channels: int, kernel_size: int, stride: int, padding: int, alpha_slope: float
+    ) -> nn.Sequential:
+        """
+        Helper method to create a convolutional block with LeakyReLU activation.
+
+        Args:
+            in_channels (int): The number of input channels to the convolutional layer.
+            out_channels (int): The number of output channels produced by the convolutional layer.
+            kernel_size (int): The size of the convolutional kernel (both height and width).
+            stride (int): The stride of the convolution operation.
+            padding (int): The amount of padding added to both sides of the input.
+            alpha_slope (float): The slope of the LeakyReLU activation function for negative inputs.
+
+        Returns:
+            nn.Sequential: A PyTorch `nn.Sequential` module containing the convolutional layer and
+                            the LeakyReLU activation function.
+        """
+
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding),
+            nn.LeakyReLU(negative_slope=alpha_slope, inplace=True)
+        )
+
+    @staticmethod
+    def _deconv_block(
+            in_channels: int, out_channels: int, kernel_size: int, stride: int, padding: int, alpha_slope: float
+    ) -> nn.Sequential:
+        """
+        Helper method to create a transposed convolutional block with LeakyReLU activation.
+
+        Helper method to create a convolutional block with LeakyReLU activation.
+
+        Args:
+            in_channels (int): The number of input channels to the convolutional layer.
+            out_channels (int): The number of output channels produced by the convolutional layer.
+            kernel_size (int): The size of the convolutional kernel (both height and width).
+            stride (int): The stride of the convolution operation.
+            padding (int): The amount of padding added to both sides of the input.
+            alpha_slope (float): The slope of the LeakyReLU activation function for negative inputs.
+
+        Returns:
+            nn.Sequential: A PyTorch `nn.Sequential` module containing the convolutional layer and
+                            the LeakyReLU activation function.
+        """
+
+        return nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding),
+            nn.LeakyReLU(negative_slope=alpha_slope, inplace=True)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass through the autoencoder.
+
+        Args:
+            x (torch.Tensor): A tensor containing the input data.
+
+        Returns:
+            torch.Tensor: A tensor containing the output data.
+        """
+
         x = self.encoder(x)
         x = self.decoder(x)
         return x
--- models/extended_model.py
+++ models/extended_model.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 
@@ -5,195 +6,100 @@ class ExtendedAutoEncoder(nn.Module):
     def __init__(self, cfg):
         super(ExtendedAutoEncoder, self).__init__()
 
+        # Extract configuration values
+        input_channel = cfg.get("input_channel")
+        flc = cfg.get("flc")
+        kernel_size = cfg.get("kernel_size")
+        stride = cfg.get("stride")
+        padding = cfg.get("padding")
+        alpha_slope = cfg.get("alpha_slope")
+        latent_space_dimension = cfg.get("latent_space_dimension")
+
+        # Encoder layers
         self.encoder = nn.Sequential(
-            nn.Conv2d(in_channels=cfg.get("input_channel"),
-                      out_channels=cfg.get("flc")[0],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[1],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope")),
-
-            nn.Conv2d(in_channels=cfg.get("flc")[0],
-                      out_channels=cfg.get("flc")[0],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[0],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope")),
-
-            nn.Conv2d(in_channels=cfg.get("flc")[0],
-                      out_channels=cfg.get("flc")[0],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[0],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope")),
-
-            nn.Conv2d(in_channels=cfg.get("flc")[0],
-                      out_channels=cfg.get("flc")[1],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[1],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope")),
-
-            nn.Conv2d(in_channels=cfg.get("flc")[1],
-                      out_channels=cfg.get("flc")[1],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[0],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope")),
-
-            nn.Conv2d(in_channels=cfg.get("flc")[1],
-                      out_channels=cfg.get("flc")[1],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[0],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope")),
-
-            nn.Conv2d(in_channels=cfg.get("flc")[1],
-                      out_channels=cfg.get("flc")[2],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[1],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope")),
-
-            nn.Conv2d(in_channels=cfg.get("flc")[2],
-                      out_channels=cfg.get("flc")[2],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[0],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope")),
-
-            nn.Conv2d(in_channels=cfg.get("flc")[2],
-                      out_channels=cfg.get("flc")[2],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[0],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope")),
-
-            nn.Conv2d(in_channels=cfg.get("flc")[2],
-                      out_channels=cfg.get("flc")[3],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[1],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope")),
-
-            nn.Conv2d(in_channels=cfg.get("flc")[3],
-                      out_channels=cfg.get("flc")[3],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[0],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope")),
-
-            nn.Conv2d(in_channels=cfg.get("flc")[3],
-                      out_channels=cfg.get("flc")[3],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[0],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope")),
-
-            nn.Conv2d(in_channels=cfg.get("flc")[3],
-                      out_channels=cfg.get("latent_space_dimension"),
-                      kernel_size=cfg.get("kernel_size")[2],
-                      stride=cfg.get("stride")[0],
-                      padding=cfg.get("padding")[0])
+            self._conv_block(input_channel, flc[0], kernel_size[0], stride[1], padding[1], alpha_slope),
+            self._conv_block(flc[0], flc[0], kernel_size[0], stride[0], padding[1], alpha_slope),
+            self._conv_block(flc[0], flc[0], kernel_size[0], stride[0], padding[1], alpha_slope),
+            self._conv_block(flc[0], flc[1], kernel_size[0], stride[1], padding[1], alpha_slope),
+            self._conv_block(flc[1], flc[1], kernel_size[0], stride[0], padding[1], alpha_slope),
+            self._conv_block(flc[1], flc[2], kernel_size[0], stride[1], padding[1], alpha_slope),
+            self._conv_block(flc[2], flc[2], kernel_size[0], stride[0], padding[1], alpha_slope),
+            self._conv_block(flc[2], flc[3], kernel_size[0], stride[1], padding[1], alpha_slope),
+            self._conv_block(flc[3], flc[3], kernel_size[0], stride[0], padding[1], alpha_slope),
+            self._conv_block(flc[3], flc[3], kernel_size[0], stride[0], padding[1], alpha_slope),
+            nn.Conv2d(flc[3], latent_space_dimension, kernel_size[1], stride[0], padding[0])
         )
 
+        # Decoder layers
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(in_channels=cfg.get("latent_space_dimension"),
-                               out_channels=cfg.get("flc")[3],
-                               kernel_size=cfg.get("kernel_size")[2],
-                               stride=cfg.get("stride")[0],
-                               padding=cfg.get("padding")[0]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope")),
-
-            nn.Conv2d(in_channels=cfg.get("flc")[3],
-                      out_channels=cfg.get("flc")[3],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[0],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope")),
-
-            nn.Conv2d(in_channels=cfg.get("flc")[3],
-                      out_channels=cfg.get("flc")[3],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[0],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope")),
-
-            nn.ConvTranspose2d(in_channels=cfg.get("flc")[3],
-                               out_channels=cfg.get("flc")[2],
-                               kernel_size=cfg.get("kernel_size")[0],
-                               stride=cfg.get("stride")[1],
-                               padding=cfg.get("padding")[1],
-                               output_padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope")),
-
-            nn.Conv2d(in_channels=cfg.get("flc")[2],
-                      out_channels=cfg.get("flc")[2],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[0],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope")),
-
-            nn.Conv2d(in_channels=cfg.get("flc")[2],
-                      out_channels=cfg.get("flc")[2],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[0],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope")),
-
-            nn.ConvTranspose2d(in_channels=cfg.get("flc")[2],
-                               out_channels=cfg.get("flc")[1],
-                               kernel_size=cfg.get("kernel_size")[0],
-                               stride=cfg.get("stride")[1],
-                               padding=cfg.get("padding")[1],
-                               output_padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope")),
-
-            nn.Conv2d(in_channels=cfg.get("flc")[1],
-                      out_channels=cfg.get("flc")[1],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[0],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope")),
-
-            nn.Conv2d(in_channels=cfg.get("flc")[1],
-                      out_channels=cfg.get("flc")[1],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[0],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope")),
-
-            nn.ConvTranspose2d(in_channels=cfg.get("flc")[1],
-                               out_channels=cfg.get("flc")[0],
-                               kernel_size=cfg.get("kernel_size")[0],
-                               stride=cfg.get("stride")[1],
-                               padding=cfg.get("padding")[1],
-                               output_padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope")),
-
-            nn.Conv2d(in_channels=cfg.get("flc")[0],
-                      out_channels=cfg.get("flc")[0],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[0],
-                      padding=cfg.get("padding")[1]),
-            nn.LeakyReLU(negative_slope=cfg.get("alpha_slope")),
-
-            nn.Conv2d(in_channels=cfg.get("flc")[0],
-                      out_channels=cfg.get("flc")[0],
-                      kernel_size=cfg.get("kernel_size")[0],
-                      stride=cfg.get("stride")[0],
-                      padding=cfg.get("padding")[1]),
-            nn.ReLU(inplace=True),
-
-            nn.ConvTranspose2d(in_channels=cfg.get("flc")[0],
-                               out_channels=cfg.get("input_channel"),
-                               kernel_size=cfg.get("kernel_size")[1],
-                               stride=cfg.get("stride")[1],
-                               padding=cfg.get("padding")[1]),
+            self._deconv_block(latent_space_dimension, flc[3], kernel_size[1], stride[0], padding[0], alpha_slope),
+            self._conv_block(flc[3], flc[3], kernel_size[0], stride[0], padding[1], alpha_slope),
+            self._conv_block(flc[3], flc[3], kernel_size[0], stride[0], padding[1], alpha_slope),
+            self._deconv_block(flc[3], flc[2], kernel_size[0], stride[1], padding[1], alpha_slope),
+            self._conv_block(flc[2], flc[2], kernel_size[0], stride[0], padding[1], alpha_slope),
+            self._deconv_block(flc[2], flc[1], kernel_size[0], stride[1], padding[1], alpha_slope),
+            self._conv_block(flc[1], flc[1], kernel_size[0], stride[0], padding[1], alpha_slope),
+            self._deconv_block(flc[1], flc[0], kernel_size[0], stride[1], padding[1], alpha_slope),
+            nn.ConvTranspose2d(flc[0], input_channel, kernel_size[1], stride=1, padding=padding[1]),
             nn.Sigmoid()
         )
 
-    def forward(self, x):
+    @staticmethod
+    def _conv_block(
+            in_channels: int, out_channels: int, kernel_size: int, stride: int, padding: int, alpha_slope: float
+    ) -> nn.Sequential:
+        """
+        Helper method to create a convolutional block with LeakyReLU activation.
+
+        Args:
+            in_channels (int): Number of input channels.
+            out_channels (int): Number of output channels.
+            kernel_size (int): Size of the convolutional kernel.
+            stride (int): Stride of the convolution.
+            padding (int): Padding applied to the input.
+            alpha_slope (float): Slope for the LeakyReLU activation function.
+
+        Returns:
+            nn.Sequential: A sequential container with convolution and LeakyReLU layers.
+        """
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding),
+            nn.LeakyReLU(negative_slope=alpha_slope, inplace=True)
+        )
+
+    @staticmethod
+    def _deconv_block(
+            in_channels: int, out_channels: int, kernel_size: int, stride: int, padding: int, alpha_slope: float
+    ) -> nn.Sequential:
+        """
+        Helper method to create a transposed convolutional block with LeakyReLU activation.
+
+        Args:
+            in_channels (int): Number of input channels.
+            out_channels (int): Number of output channels.
+            kernel_size (int): Size of the transposed convolutional kernel.
+            stride (int): Stride of the transposed convolution.
+            padding (int): Padding applied to the input.
+            alpha_slope (float): Slope for the LeakyReLU activation function.
+
+        Returns:
+            nn.Sequential: A sequential container with transposed convolution and LeakyReLU layers.
+        """
+        return nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding),
+            nn.LeakyReLU(negative_slope=alpha_slope, inplace=True)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass through the autoencoder.
+
+        Args:
+            x (torch.Tensor): Input tensor.
+
+        Returns:
+            torch.Tensor: Output tensor.
+        """
         x = self.encoder(x)
         x = self.decoder(x)
         return x
