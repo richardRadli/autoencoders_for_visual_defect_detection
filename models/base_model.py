9fe3e816939d76dbedf6ea7891d30b4da096d625
Richárd Rádli <77007078+richardRadli@users.noreply.github.com>
2024-08-10 07:18:45 +0200

    Updated models.

--- models/base_model.py
+++ models/base_model.py
@@ -38,8 +38,7 @@ class AutoEncoder(nn.Module):
             self._deconv_block(flc[1], flc[0], kernel_size[1], stride[1], padding[1], alpha_slope),
             self._conv_block(flc[0], flc[0], kernel_size[0], stride[0], padding[1], alpha_slope),
             self._deconv_block(flc[0], flc[0], kernel_size[1], stride[1], padding[1], alpha_slope),
-            nn.ConvTranspose2d(flc[0], input_channel, kernel_size[1], stride[1], padding[1]),
-            nn.Sigmoid()
+            self._last_deconv_block(flc[0], input_channel, kernel_size[1], stride[1], padding[1])
         )
 
     @staticmethod
@@ -92,6 +91,32 @@ class AutoEncoder(nn.Module):
         return nn.Sequential(
             nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding),
             nn.LeakyReLU(negative_slope=alpha_slope, inplace=True)
+        )
+
+    @staticmethod
+    def _last_deconv_block(
+            in_channels: int, out_channels: int, kernel_size: int, stride: int, padding: int
+    ) -> nn.Sequential:
+        """
+        Helper method to create a transposed convolutional block with Sigmoid activation.
+
+        Helper method to create a convolutional block with LeakyReLU activation.
+
+        Args:
+            in_channels (int): The number of input channels to the convolutional layer.
+            out_channels (int): The number of output channels produced by the convolutional layer.
+            kernel_size (int): The size of the convolutional kernel (both height and width).
+            stride (int): The stride of the convolution operation.
+            padding (int): The amount of padding added to both sides of the input.
+
+        Returns:
+            nn.Sequential: A PyTorch `nn.Sequential` module containing the convolutional layer and
+                            the Sigmoid activation function.
+        """
+
+        return nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding),
+            nn.Sigmoid()
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
--- models/extended_model.py
+++ models/extended_model.py
@@ -1,4 +1,3 @@
-import torch
 import torch.nn as nn
 
 
@@ -15,33 +14,36 @@ class ExtendedAutoEncoder(nn.Module):
         alpha_slope = cfg.get("alpha_slope")
         latent_space_dimension = cfg.get("latent_space_dimension")
 
-        # Encoder layers
         self.encoder = nn.Sequential(
             self._conv_block(input_channel, flc[0], kernel_size[0], stride[1], padding[1], alpha_slope),
             self._conv_block(flc[0], flc[0], kernel_size[0], stride[0], padding[1], alpha_slope),
             self._conv_block(flc[0], flc[0], kernel_size[0], stride[0], padding[1], alpha_slope),
             self._conv_block(flc[0], flc[1], kernel_size[0], stride[1], padding[1], alpha_slope),
             self._conv_block(flc[1], flc[1], kernel_size[0], stride[0], padding[1], alpha_slope),
+            self._conv_block(flc[1], flc[1], kernel_size[0], stride[0], padding[1], alpha_slope),
             self._conv_block(flc[1], flc[2], kernel_size[0], stride[1], padding[1], alpha_slope),
+            self._conv_block(flc[2], flc[2], kernel_size[0], stride[0], padding[1], alpha_slope),
             self._conv_block(flc[2], flc[2], kernel_size[0], stride[0], padding[1], alpha_slope),
             self._conv_block(flc[2], flc[3], kernel_size[0], stride[1], padding[1], alpha_slope),
             self._conv_block(flc[3], flc[3], kernel_size[0], stride[0], padding[1], alpha_slope),
             self._conv_block(flc[3], flc[3], kernel_size[0], stride[0], padding[1], alpha_slope),
-            nn.Conv2d(flc[3], latent_space_dimension, kernel_size[1], stride[0], padding[0])
+            nn.Conv2d(flc[3], latent_space_dimension, kernel_size[2], stride[0], padding[0]),
         )
 
-        # Decoder layers
         self.decoder = nn.Sequential(
-            self._deconv_block(latent_space_dimension, flc[3], kernel_size[1], stride[0], padding[0], alpha_slope),
+            self._deconv_block(latent_space_dimension, flc[3], kernel_size[2], stride[0], padding[0], alpha_slope),
             self._conv_block(flc[3], flc[3], kernel_size[0], stride[0], padding[1], alpha_slope),
             self._conv_block(flc[3], flc[3], kernel_size[0], stride[0], padding[1], alpha_slope),
             self._deconv_block(flc[3], flc[2], kernel_size[0], stride[1], padding[1], alpha_slope),
             self._conv_block(flc[2], flc[2], kernel_size[0], stride[0], padding[1], alpha_slope),
+            self._conv_block(flc[2], flc[2], kernel_size[0], stride[0], padding[1], alpha_slope),
             self._deconv_block(flc[2], flc[1], kernel_size[0], stride[1], padding[1], alpha_slope),
             self._conv_block(flc[1], flc[1], kernel_size[0], stride[0], padding[1], alpha_slope),
+            self._conv_block(flc[1], flc[1], kernel_size[0], stride[0], padding[1], alpha_slope),
             self._deconv_block(flc[1], flc[0], kernel_size[0], stride[1], padding[1], alpha_slope),
-            nn.ConvTranspose2d(flc[0], input_channel, kernel_size[1], stride=1, padding=padding[1]),
-            nn.Sigmoid()
+            self._conv_block(flc[0], flc[0], kernel_size[0], stride[0], padding[1], alpha_slope),
+            self._conv_block(flc[0], flc[0], kernel_size[0], stride[0], padding[1], alpha_slope),
+            self._last_deconv_block(flc[0], input_channel, kernel_size[1], stride[1], padding[1])
         )
 
     @staticmethod
@@ -52,16 +54,18 @@ class ExtendedAutoEncoder(nn.Module):
         Helper method to create a convolutional block with LeakyReLU activation.
 
         Args:
-            in_channels (int): Number of input channels.
-            out_channels (int): Number of output channels.
-            kernel_size (int): Size of the convolutional kernel.
-            stride (int): Stride of the convolution.
-            padding (int): Padding applied to the input.
-            alpha_slope (float): Slope for the LeakyReLU activation function.
+            in_channels (int): The number of input channels to the convolutional layer.
+            out_channels (int): The number of output channels produced by the convolutional layer.
+            kernel_size (int): The size of the convolutional kernel (both height and width).
+            stride (int): The stride of the convolution operation.
+            padding (int): The amount of padding added to both sides of the input.
+            alpha_slope (float): The slope of the LeakyReLU activation function for negative inputs.
 
         Returns:
-            nn.Sequential: A sequential container with convolution and LeakyReLU layers.
+            nn.Sequential: A PyTorch `nn.Sequential` module containing the convolutional layer and
+                            the LeakyReLU activation function.
         """
+
         return nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding),
             nn.LeakyReLU(negative_slope=alpha_slope, inplace=True)
@@ -74,32 +78,53 @@ class ExtendedAutoEncoder(nn.Module):
         """
         Helper method to create a transposed convolutional block with LeakyReLU activation.
 
+        Helper method to create a convolutional block with LeakyReLU activation.
+
         Args:
-            in_channels (int): Number of input channels.
-            out_channels (int): Number of output channels.
-            kernel_size (int): Size of the transposed convolutional kernel.
-            stride (int): Stride of the transposed convolution.
-            padding (int): Padding applied to the input.
-            alpha_slope (float): Slope for the LeakyReLU activation function.
+            in_channels (int): The number of input channels to the convolutional layer.
+            out_channels (int): The number of output channels produced by the convolutional layer.
+            kernel_size (int): The size of the convolutional kernel (both height and width).
+            stride (int): The stride of the convolution operation.
+            padding (int): The amount of padding added to both sides of the input.
+            alpha_slope (float): The slope of the LeakyReLU activation function for negative inputs.
 
         Returns:
-            nn.Sequential: A sequential container with transposed convolution and LeakyReLU layers.
+            nn.Sequential: A PyTorch `nn.Sequential` module containing the convolutional layer and
+                            the LeakyReLU activation function.
         """
+
         return nn.Sequential(
-            nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding),
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, output_padding=padding),
             nn.LeakyReLU(negative_slope=alpha_slope, inplace=True)
         )
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    @staticmethod
+    def _last_deconv_block(
+            in_channels: int, out_channels: int, kernel_size: int, stride: int, padding: int
+    ) -> nn.Sequential:
         """
-        Forward pass through the autoencoder.
+        Helper method to create a transposed convolutional block with Sigmoid activation.
+
+        Helper method to create a convolutional block with LeakyReLU activation.
 
         Args:
-            x (torch.Tensor): Input tensor.
+            in_channels (int): The number of input channels to the convolutional layer.
+            out_channels (int): The number of output channels produced by the convolutional layer.
+            kernel_size (int): The size of the convolutional kernel (both height and width).
+            stride (int): The stride of the convolution operation.
+            padding (int): The amount of padding added to both sides of the input.
 
         Returns:
-            torch.Tensor: Output tensor.
+            nn.Sequential: A PyTorch `nn.Sequential` module containing the convolutional layer and
+                            the Sigmoid activation function.
         """
+
+        return nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
         x = self.encoder(x)
         x = self.decoder(x)
         return x
